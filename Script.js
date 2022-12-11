b0369233298661703034b38dd30b30b75f6238f0
LAPTOP-EJKR5DPM\Admin <vigneshsurya21796@gmail.com>
2022-12-11 22:27:10 +0530

    Animated Search bar

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
-console.log('completed');
+const input1 = document.querySelector('.wrapper');
+const input2 = document.querySelector('.input');
+const input3 = document.querySelector('.search');
+
+function clicevent() {
+  console.log('cliked');
+  input2.classList.remove('condition');
+  input3.classList.add('condition');
+}
+function remove() {
+//   console.log('cliked');
+  input2.classList.add('condition');
+  input3.classList.remove('condition');
+}
