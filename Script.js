d7be605254232209c4306dfafce8cb3630668a26
LAPTOP-EJKR5DPM\Admin <vigneshsurya21796@gmail.com>
2022-12-26 13:26:31 +0530

    Side Bar -version 1

--- Script.js
+++ Script.js
@@ -1,14 +1,15 @@
-const input1 = document.querySelector('.wrapper');
-const input2 = document.querySelector('.input');
-const input3 = document.querySelector('.search');
+const input1 = document.querySelector('.Wrapper');
+const input2 = document.querySelector('.Close');
+const input3 = document.querySelector('.Menu');
 
 function clicevent() {
   console.log('cliked');
-  input2.classList.remove('condition');
-  input3.classList.add('condition');
+  input1.classList.remove('none');
+  // input3.classList.add('condition');
 }
 function remove() {
-//   console.log('cliked');
-  input2.classList.add('condition');
-  input3.classList.remove('condition');
+  input1.classList.add('none');
+  //   console.log('cliked');
+  // input2.classList.add('condition');
+  // input3.classList.remove('condition');
 }
