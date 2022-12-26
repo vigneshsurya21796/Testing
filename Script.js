79f1a763819121eaf287363dfbc383cb39a55189
LAPTOP-EJKR5DPM\Admin <vigneshsurya21796@gmail.com>
2022-12-26 17:09:43 +0530

    Side bar-version 2

--- Script.js
+++ Script.js
@@ -1,7 +1,8 @@
 const input1 = document.querySelector('.Wrapper');
 const input2 = document.querySelector('.Close');
 const input3 = document.querySelector('.Menu');
-
+const input4 = document.querySelector('.itemvalue');
+const input5 = document.querySelector('.down-icon');
 function clicevent() {
   console.log('cliked');
   input1.classList.remove('none');
@@ -9,6 +10,13 @@ function clicevent() {
 }
 function remove() {
   input1.classList.add('none');
+  //   console.log('cliked');
+  // input2.classList.add('condition');
+  // input3.classList.remove('condition');
+}
+function toggle() {
+  input4.classList.toggle('none');
+  input5.classList.toggle('rotate');
   //   console.log('cliked');
   // input2.classList.add('condition');
   // input3.classList.remove('condition');
