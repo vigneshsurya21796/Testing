e4d867454cb1f8219e96b2b046b2baa160cbe1ac
LAPTOP-EJKR5DPM\Admin <vigneshsurya21796@gmail.com>
2023-01-17 10:16:42 +0530

    Sidabar version-another

--- Script.js
+++ Script.js
@@ -3,16 +3,23 @@ const input2 = document.querySelector('.Close');
 const input3 = document.querySelector('.Menu');
 const input4 = document.querySelector('.itemvalue');
 const input5 = document.querySelector('.down-icon');
+const input6 = document.querySelector('.toggle-wrapper');
+const input7 = document.querySelector('.icon-down');
+const input8 = document.querySelector('.toggle_1-wrapper');
+const input9 = document.querySelector('.icon-down_1');
+
 function clicevent() {
   console.log('cliked');
-  input1.classList.remove('none');
+  input1.classList.remove('display');
+  input3.classList.add('display');
+
   // input3.classList.add('condition');
 }
 function remove() {
-  input1.classList.add('none');
+  input1.classList.add('display');
   //   console.log('cliked');
   // input2.classList.add('condition');
-  // input3.classList.remove('condition');
+  input3.classList.remove('display');
 }
 function toggle() {
   input4.classList.toggle('none');
@@ -20,4 +27,27 @@ function toggle() {
   //   console.log('cliked');
   // input2.classList.add('condition');
   // input3.classList.remove('condition');
+}
+function toggle_1() {
+  input6.classList.toggle('display');
+  input7.classList.toggle('icon-down-1');
+
+  if (!input7.classList.contains('icon-down-1')) {
+    input7.classList.toggle('icon-down-2');
+  }
+  if (input7.classList.contains('icon-down-1')) {
+    input7.classList.remove('icon-down-2');
+  }
+}
+
+function toggle_2() {
+  input8.classList.toggle('display');
+  input9.classList.toggle('icon-down-1');
+
+  if (!input9.classList.contains('icon-down-1')) {
+    input9.classList.toggle('icon-down-2');
+  }
+  if (input9.classList.contains('icon-down-1')) {
+    input9.classList.remove('icon-down-2');
+  }
 }
